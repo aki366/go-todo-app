3f39a255d9c2d6f91bfda9a86bc81fe2553c5706
aki366 <akihiro.okuda366@gmail.com>
2024-04-22 13:32:04 +0900

    ルーティング実装

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "todo-app/controllers"
+)
+
+func main() {
+		gin.SetMode(gin.ReleaseMode)
+    router := gin.Default()
+
+		// ルートパスのハンドラーを追加
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+					"message": "Welcome to the ToDo API!",
+			})
+		})
+
+    router.GET("/todos", controllers.GetTodos)
+    router.POST("/todos", controllers.AddTodo)
+    router.DELETE("/todos/:id", controllers.DeleteTodo)
+
+    router.Run(":8080")
+}
